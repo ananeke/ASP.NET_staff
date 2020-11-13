931cdefc6e4820a7ce586374092b85f4acdccb54
Dorota Przyjemska-Krusińska <ananeke@gmail.com>
2020-11-13 13:13:13 +0100

    FirstAPP of ASP.NET
    app asp.net with entityframework and razor

---
+++ Rocky/Rocky/Controllers/ApplicationTypeController .cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ApplicationTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ApplicationType> objList= _db.ApplicationType;
+            return View(objList);
+        }
+        /// <summary>
+        /// Get - Create
+        /// </summary>
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        /// <summary>
+        /// Post - Create
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ApplicationType obj)
+        {
+            _db.ApplicationType.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ Rocky/Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objListOfCategory = _db.Category;
+            return View(objListOfCategory);
+        }
+        /// <summary>
+        /// Get - Create
+        /// </summary>
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+        /// <summary>
+        /// Post - Create
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            
+            return View(category);
+        }
+    }
+
+}
